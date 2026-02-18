7274011ef16aab848d086e8cd954211d5a4d118e
yomi <yinkom0708@gmail.com>
2026-02-18 17:18:14 +0900

    fix: publish error to npm

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ import {
 import { enableDebug, debug } from './utils/debug.js';
 import { getGlobalOptionsTable } from './utils/help.js';
 
-const VERSION = '1.0.3';
+const VERSION = '1.0.5';
 
 // ASCII art logo
 const logo = chalk.cyan(`
